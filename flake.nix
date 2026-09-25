159090b24b88c992e8068f782bd08807e4736c49
L.Yang <yang120120110@gmail.com>
2026-09-26 03:50:00 +0800

    Update dev shell for ROCm 7.2

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "ComfyUI - NixOS + TheRock ROCm 10";
+  description = "ComfyUI - NixOS + ROCm 7.2";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs =
     { self, nixpkgs }:
@@ -13,25 +13,30 @@
           git
           python313Packages.pip
           ruff
+
           stdenv.cc.cc.lib
           zlib
+          zstd
+          glib
           glibc
           numactl
         ];
-        # TheRock 的 pip 轮子自带 rocm，不需要 rocmPackages.clr
+
         shellHook = ''
           # 让 .venv 里的 python 优先
           export UV_PYTHON=3.13
-          export HSA_OVERRIDE_GFX_VERSION=10.3.0
+          # export HSA_OVERRIDE_GFX_VERSION=10.3.0
           # 完善动态链接库路径，加入 numactl 和系统 C 库
-          export LD_LIBRARY_PATH=${
+          export LD_LIBRARY_PATH="${
             pkgs.lib.makeLibraryPath [
               pkgs.stdenv.cc.cc.lib
               pkgs.zlib
+              pkgs.zstd
+              pkgs.glib
               pkgs.numactl
               pkgs.glibc
             ]
-          }:$LD_LIBRARY_PATH
+          }''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
         '';
       };
